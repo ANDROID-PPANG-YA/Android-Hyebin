9b826960de6f427ab6457f948dba5ca199b36e6c
lhb8106 <lhb8106@naver.com>
2022-04-06 23:56:53 +0900

    init setting

---
+++ app/src/main/java/com/sopt/anroid_hyebin/presentation/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.anroid_hyebin.presentation.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sopt.anroid_hyebin.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/sopt/anroid_hyebin/presentation/sign/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.anroid_hyebin.presentation.sign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sopt.anroid_hyebin.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
