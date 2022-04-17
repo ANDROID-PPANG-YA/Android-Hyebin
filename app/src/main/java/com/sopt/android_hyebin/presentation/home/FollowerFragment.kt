e448bb13a65dd40d0a92568e8dc33d5e14d163c5
lhb8106 <lhb8106@naver.com>
2022-04-18 01:16:43 +0900

    [FEAT] #5 - basefragment setting

---
+++ app/src/main/java/com/sopt/android_hyebin/presentation/home/FollowerFragment.kt
@@ -0,0 +1,17 @@
+package com.sopt.android_hyebin.presentation.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sopt.android_hyebin.R
+import com.sopt.android_hyebin.databinding.FragmentFollowerBinding
+import com.sopt.anroid_hyebin.util.BaseFragment
+
+class FollowerFragment : BaseFragment<FragmentFollowerBinding>(R.layout.fragment_repository) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/sopt/android_hyebin/presentation/home/RepositoryFragment.kt
@@ -0,0 +1,19 @@
+package com.sopt.android_hyebin.presentation.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sopt.android_hyebin.R
+import com.sopt.android_hyebin.databinding.FragmentFollowerBinding
+import com.sopt.android_hyebin.databinding.FragmentRepositoryBinding
+import com.sopt.anroid_hyebin.util.BaseFragment
+
+class RepositoryFragment : BaseFragment<FragmentRepositoryBinding>(R.layout.fragment_repository) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
