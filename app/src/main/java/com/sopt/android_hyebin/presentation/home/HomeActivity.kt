c49f96a30833a4aed4a47f5f57b121cfe7571adb
lhb8106 <lhb8106@naver.com>
2022-04-19 20:41:28 +0900

    [FEAT] #5 fragment transaction

--- app/src/main/java/com/sopt/android_hyebin/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/sopt/android_hyebin/presentation/home/HomeActivity.kt
@@ -1,7 +1,9 @@
 package com.sopt.android_hyebin.presentation.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
 import com.sopt.android_hyebin.R
 import com.sopt.android_hyebin.databinding.ActivityHomeBinding
 import com.sopt.anroid_hyebin.presentation.home.HomeViewModel
@@ -9,16 +11,56 @@ import com.sopt.anroid_hyebin.util.BaseActivity
 
 class HomeActivity :
     BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
-
+    private var postion = FIRST_POSITION
     private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initViewModel()
+        initTransactionEvent()
     }
 
     private fun initViewModel() {
         binding.data = viewModel
+    }
+
+    fun initTransactionEvent() {
+        val followerFragment = FollowerFragment()
+        val repositstoryFragment = RepositoryFragment()
+
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container, followerFragment).commit()
+
+        binding.tvFollower.setOnClickListener {
+            //supportFragmentManager.beginTransaction().replace(R.id.fragment_container, followerFragment).commit()
+            val transaction = supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container, followerFragment)
+                addToBackStack(null)
+            }
+
+            transaction.commit();
+        }
+
+        binding.tvRepository.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container, repositstoryFragment)
+                addToBackStack(null)
+            }
+
+            transaction.commit();
+            //supportFragmentManager.beginTransaction().replace(R.id.fragment_container, repositstoryFragment).commit()
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+
+
+    companion object {
+        const val FIRST_POSITION = 1
     }
 
 }
--- app/src/main/java/com/sopt/android_hyebin/presentation/home/RepositoryFragment.kt
+++ app/src/main/java/com/sopt/android_hyebin/presentation/home/RepositoryFragment.kt
@@ -1,12 +1,10 @@
 package com.sopt.android_hyebin.presentation.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.sopt.android_hyebin.R
-import com.sopt.android_hyebin.databinding.FragmentFollowerBinding
 import com.sopt.android_hyebin.databinding.FragmentRepositoryBinding
 import com.sopt.anroid_hyebin.util.BaseFragment
 
@@ -16,4 +14,5 @@ class RepositoryFragment : BaseFragment<FragmentRepositoryBinding>(R.layout.frag
         super.onViewCreated(view, savedInstanceState)
 
     }
+
 }
