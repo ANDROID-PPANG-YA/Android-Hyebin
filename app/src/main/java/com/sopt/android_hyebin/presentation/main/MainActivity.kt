240953dd2300079985f9bc0c49abefd11d84bd45
lhb8106 <lhb8106@naver.com>
2022-04-29 03:16:09 +0900

    [FEAT] #7 - add bottom navi

--- app/src/main/java/com/sopt/android_hyebin/presentation/main/MainActivity.kt
+++ app/src/main/java/com/sopt/android_hyebin/presentation/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sopt.android_hyebin.presentation.main
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.fragment.app.viewModels
+import androidx.viewpager2.widget.ViewPager2
 import com.sopt.android_hyebin.R
 import com.sopt.android_hyebin.databinding.ActivityMainBinding
 import com.sopt.android_hyebin.databinding.FragmentProfileBinding
@@ -24,6 +25,7 @@ class MainActivity :
         super.onCreate(savedInstanceState)
 
         initAdapter()
+        initBottomNavi()
     }
 
     private fun initAdapter() {
@@ -32,6 +34,38 @@ class MainActivity :
         mainViewPagerAdapter.fragments.addAll(fragmentList)
 
         binding.vpMain.adapter = mainViewPagerAdapter
+    }
+
+
+    private fun initBottomNavi() {
+        binding.vpMain.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                binding.bnvMain.menu.getItem(position).isChecked = true
+            }
+        })
+
+        binding.bnvMain.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.menu_profile -> {
+                    binding.vpMain.currentItem= FIRST_FRAGMENT
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu_home -> {
+                    binding.vpMain.currentItem= SECOND_FRAGMENT
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    binding.vpMain.currentItem= THIRD_FRAGMENT
+                    return@setOnItemSelectedListener true
+                }
+            }
+        }
+    }
+
+    companion object{
+        const val FIRST_FRAGMENT = 0
+        const val SECOND_FRAGMENT = 1
+        const val THIRD_FRAGMENT = 2
     }
 
 
